ad3796ef0025df07e36b854ad47e8ccc7ba9d417
Aasem <assem_soma541@yahoo.com>
2021-12-06 02:25:04 +0200

    Handling Login State in Toast Message

--- lib/modules/login/cubit/login_cubit.dart
+++ lib/modules/login/cubit/login_cubit.dart
@@ -3,6 +3,8 @@ import 'package:eshtri/models/login_model.dart';
 import 'package:eshtri/modules/login/cubit/login_states.dart';
 import 'package:eshtri/shared/network/end_points.dart';
 import 'package:eshtri/shared/network/remote/dio_helper.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class LoginCubit extends Cubit<LoginStates> {
   LoginCubit() : super(LoginInitialState());
@@ -13,13 +15,29 @@ class LoginCubit extends Cubit<LoginStates> {
       final response = await DioHelper.postRequest(
           path: kLoginEndPoint, data: {'email': email, 'password': password});
       final loginModel = LoginModel.fromJson(response.data);
-      emit(LoginSuccessState());
-      print(loginModel.data!.name);
-      print(loginModel.message);
-      print(loginModel.status);
+      if (loginModel.status) {
+        Fluttertoast.showToast(
+            msg: loginModel.message,
+            toastLength: Toast.LENGTH_LONG,
+            gravity: ToastGravity.BOTTOM,
+            timeInSecForIosWeb: 5,
+            backgroundColor: Colors.green,
+            textColor: Colors.white,
+            fontSize: 16.0);
+        emit(LoginSuccessState());
+      } else {
+        Fluttertoast.showToast(
+            msg: loginModel.message,
+            toastLength: Toast.LENGTH_LONG,
+            gravity: ToastGravity.BOTTOM,
+            timeInSecForIosWeb: 5,
+            backgroundColor: Colors.red,
+            textColor: Colors.white,
+            fontSize: 16.0);
+        emit(LoginFailState());
+      }
     } catch (error) {
       emit(LoginFailState());
-      print(error.toString());
     }
   }
 }
